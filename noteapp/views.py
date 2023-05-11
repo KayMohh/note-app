89c3cc7738c2d1e0fe23963ebf0012c6baccc1e1
kaymohh <thrilltim@gmail.com>
2023-05-11 17:25:19 +0100

    pycache

--- noteapp/views.py
+++ noteapp/views.py
@@ -1,3 +1,9 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 views = Blueprint('views', __name__)
+
+
+@views.route('/')
+@views.route('/home')
+def home():
+    return "<h1>Help</h1>"
