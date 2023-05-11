85cf193b154380d957c58255c3c8e5a9af4d4e57
kaymohh <thrilltim@gmail.com>
2023-05-11 17:02:51 +0100

    views.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from noteapp import create_app
+
 app = create_app()
 
 if __name__ == '__main__':
--- noteapp/__init__.py
+++ noteapp/__init__.py
@@ -5,6 +5,4 @@ def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'foodisgood'
 
-
-
     return app
